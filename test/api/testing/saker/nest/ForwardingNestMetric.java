4461c6072b1a02be3e29729e5970f00ba5ed5d37
sipkab <10866741+Sipkab@users.noreply.github.com>
2020-01-30 14:07:56 +0100

    Allow null metric for ForwardingNestMetric

--- test/api/testing/saker/nest/ForwardingNestMetric.java
+++ test/api/testing/saker/nest/ForwardingNestMetric.java
@@ -21,60 +21,75 @@ public class ForwardingNestMetric implements NestMetric {
 
 	@Override
 	public Integer getServerRequestResponseCode(String requesturl) throws IOException {
-		return metric.getServerRequestResponseCode(requesturl);
+		return metric != null ? metric.getServerRequestResponseCode(requesturl)
+				: NestMetric.super.getServerRequestResponseCode(requesturl);
 	}
 
 	@Override
 	public InputStream getServerRequestResponseStream(String requesturl) throws IOException {
-		return metric.getServerRequestResponseStream(requesturl);
+		return metric != null ? metric.getServerRequestResponseStream(requesturl)
+				: NestMetric.super.getServerRequestResponseStream(requesturl);
 	}
 
 	@Override
 	public InputStream getServerRequestResponseErrorStream(String requesturl) throws IOException {
-		return metric.getServerRequestResponseErrorStream(requesturl);
+		return metric != null ? metric.getServerRequestResponseErrorStream(requesturl)
+				: NestMetric.super.getServerRequestResponseErrorStream(requesturl);
 	}
 
 	@Override
 	public Map<String, String> getServerRequestResponseHeaders(String requesturl) {
-		return metric.getServerRequestResponseHeaders(requesturl);
+		return metric != null ? metric.getServerRequestResponseHeaders(requesturl)
+				: NestMetric.super.getServerRequestResponseHeaders(requesturl);
 	}
 
 	@Override
 	public PublicKey overrideServerBundleSignaturePublicKey(String server, int version) {
-		return metric.overrideServerBundleSignaturePublicKey(server, version);
+		return metric != null ? metric.overrideServerBundleSignaturePublicKey(server, version)
+				: NestMetric.super.overrideServerBundleSignaturePublicKey(server, version);
 	}
 
 	@Override
 	public boolean allowCachedVerificationState(String bundleid) {
-		return metric.allowCachedVerificationState(bundleid);
+		return metric != null ? metric.allowCachedVerificationState(bundleid)
+				: NestMetric.super.allowCachedVerificationState(bundleid);
 	}
 
 	@Override
 	public FileHashResult overrideParameterBundlePerceivedHash(SakerFileProvider fp, SakerPath bundlepath,
 			String algorithm) throws NoSuchAlgorithmException, IOException {
-		return metric.overrideParameterBundlePerceivedHash(fp, bundlepath, algorithm);
+		return metric != null ? metric.overrideParameterBundlePerceivedHash(fp, bundlepath, algorithm)
+				: NestMetric.super.overrideParameterBundlePerceivedHash(fp, bundlepath, algorithm);
 	}
 
 	@Override
 	public boolean offerConfiguredRepositoryCache(Object repositoryobj, String repoid,
 			ExecutionPathConfiguration pathconfig, Map<String, String> userparameters, Object configuredstorage) {
-		return metric.offerConfiguredRepositoryCache(repositoryobj, repoid, pathconfig, userparameters,
-				configuredstorage);
+		return metric != null
+				? metric.offerConfiguredRepositoryCache(repositoryobj, repoid, pathconfig, userparameters,
+						configuredstorage)
+				: NestMetric.super.offerConfiguredRepositoryCache(repositoryobj, repoid, pathconfig, userparameters,
+						configuredstorage);
 	}
 
 	@Override
 	public Object retrieveCachedConfiguredRepository(Object repositoryobj, String repoid,
 			ExecutionPathConfiguration pathconfig, Map<String, String> userparameters) {
-		return metric.retrieveCachedConfiguredRepository(repositoryobj, repoid, pathconfig, userparameters);
+		return metric != null
+				? metric.retrieveCachedConfiguredRepository(repositoryobj, repoid, pathconfig, userparameters)
+				: NestMetric.super.retrieveCachedConfiguredRepository(repositoryobj, repoid, pathconfig,
+						userparameters);
 	}
 
 	@Override
 	public Boolean overrideServerUncacheRequestsValue() {
-		return metric.overrideServerUncacheRequestsValue();
+		return metric != null ? metric.overrideServerUncacheRequestsValue()
+				: NestMetric.super.overrideServerUncacheRequestsValue();
 	}
 
 	@Override
 	public Path overrideNativeLibraryPath(ClassLoader cl, Path libpath) {
-		return metric.overrideNativeLibraryPath(cl, libpath);
+		return metric != null ? metric.overrideNativeLibraryPath(cl, libpath)
+				: NestMetric.super.overrideNativeLibraryPath(cl, libpath);
 	}
 }
